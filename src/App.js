492c43577f98ad90ab25d2d0a2d1ccd75d87323a
Dammy36 <113221009+Dammy36@users.noreply.github.com>
2023-02-08 17:10:35 +0100

    Added the footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 export default App;
 function App() {
   return (
-    <div className='container'>Hello world</div>
+    <div className='container'>Hello world
+      <footer>
+        This project is created by Oseyemi Dammy and is {}
+        <a href='https://github.com/Dammy36/weather3-react'>Opensourced on Github</a> and { }
+        <a href='https://unrivaled-cucurucho-9ba8c8.netlify.app/'>Hosted on Netlify </a>
+    </footer>
+    
+    </div>
+    
   );
 }
 
