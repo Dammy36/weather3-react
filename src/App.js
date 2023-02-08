4c9492d32a507bdcbf17598accd8b80b109ce42a
Dammy36 <113221009+Dammy36@users.noreply.github.com>
2023-02-08 17:28:33 +0100

    created a new component called weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Weather from './Weather';
 import './App.css';
 export default App;
 function App() {
-  return (
-    <div className='container'>Hello world
+    return (
+      <div className='container'>Hello world
+        <Weather/>
       <footer>
         This project is created by Oseyemi Dammy and is {}
         <a href='https://github.com/Dammy36/weather3-react'>Opensourced on Github</a> and { }
