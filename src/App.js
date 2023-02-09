47e9274b8bfcd89e0085bc2d63954f1a615037a3
Dammy36 <113221009+Dammy36@users.noreply.github.com>
2023-02-09 13:50:39 +0100

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import Weather from './Weather';
 import './App.css';
+
 export default App;
 function App() {
     return (
-      <div className='container'>Hello world
+      <div className='container'>
+        <div className='App'>
         <Weather/>
       <footer>
-        This project is created by Oseyemi Dammy and is {}
+        This project is created by Oseyemi Dammy, is {}
         <a href='https://github.com/Dammy36/weather3-react'>Opensourced on Github</a> and { }
         <a href='https://unrivaled-cucurucho-9ba8c8.netlify.app/'>Hosted on Netlify </a>
     </footer>
     
     </div>
-    
+    </div>
   );
 }
 
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,48 @@
-import React from "react"
+import React from "react";
+import "./weather.css";
+
 export default function Weather() {
     return (
         <div className="weather" >
-            <h1>WEATHER APP</h1>
+            <form>
+            <div className="row">
+                <div className="col-sm-9">
+                        <input type="search" placeholder="Enter a city" className="form-input"
+            autoFocus="on"/>
+                </div>
+                <div className="col-sm-3">
+                    <input type="submit" value="search" className="btn btn-primary w-100" />
+                    </div>
+                </div>
+                </form>
+            <h1>Lagos</h1>
+            <ul>
+                <li>Wednesday 5:00</li>
+                <li>Partly  cloudy</li>
+            </ul>
+            <div className="row mt-3">
+                <div className="col-sm-6"> 
+                 <div className="clearfix">
+                        <img
+                            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="partly cloudy"                            
+                       className="float-left"
+                        />
+                    
+                    <span className="temperature">21</span>
+                            <span className="unit">°C</span>           
+                                               
+                </div>
+                </div>
+                <div className="col-sm-6">
+                    <ul>
+                        <li>prescipition:15</li>
+                        <li>Humidity:72%</li>
+                        <li>Wind:13km/h</li>
+                    </ul>
+                    
+                </div>
+            </div>
+
        </div>
     )
 }
